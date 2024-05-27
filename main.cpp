d9afddb63359cc5ad0ff9da62f7fc68df0ac96f5
JaneBabiakA <janefrances369@gmail.com>
2024-05-26 21:06:47 -0400

    Added basic parser functionality for existing token types

--- main.cpp
+++ main.cpp
@@ -16,11 +16,22 @@ int main(int argc, char* argv[]) {
     content << file.rdbuf();
     std::cout << content.str() << std::endl;
     Lexer lexer = Lexer(content.str());
-    std::vector<Token> output = lexer.run();
+    std::vector<Token> tokens = lexer.run();
 
     //temporarily print out tokens to test
-    for(Token t : output){
+    for(Token t : tokens){
         std::cout << *t.value << std::endl;
+    }
+
+    Parser parser = Parser(tokens);
+    ReturnNode tree = parser.run();
+
+    //temporarily print out 'tree' to test
+    if(tree.expr.has_value()){
+        std::cout << *(tree.expr.value().expr).value << std::endl;
+    }
+    else{
+        std::cout << "no return value" << std::endl;
     }
     return 0;
 }
--- parser.h
+++ parser.h
@@ -1,1 +1,36 @@
 #pragma once
+
+#include<iostream>
+#include<fstream>
+#include<sstream>
+#include <utility>
+#include <vector>
+#include<optional>
+
+struct IntegerNode{
+    Token expr;
+};
+
+struct ReturnNode{
+    std::optional<IntegerNode> expr;
+};
+
+class Parser{
+public:
+    explicit Parser(std::vector<Token> tokens){
+        m_tokens = std::move(tokens);
+    }
+
+    ReturnNode run(){
+        ReturnNode returnNode;
+        for(int index = 0; index < m_tokens.size(); index++){
+            if((m_tokens[index].type == TokenTypes::_return) && (index + 1 < m_tokens.size()) && (m_tokens[index + 1].type != TokenTypes::_semi)){
+                returnNode.expr = IntegerNode({m_tokens.at(index + 1)});
+            }
+        }
+        return returnNode;
+    }
+
+private:
+    std::vector<Token> m_tokens;
+};
